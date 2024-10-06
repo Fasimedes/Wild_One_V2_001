9426544ac21867e418086e5f5a7952ffbcd473a8
petrl <petrl@DESKTOP-48V4KVH>
2024-10-06 20:25:27 +0200

    Add project files.

---
+++ Wild_One_V2_001/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Wild_One_V2_001
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
